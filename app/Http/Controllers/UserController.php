a92f75715903d7e09cf63c31feafbeb0f514316a
EIEV <eiev.alwathba.notif@gmail.com>
2022-11-15 06:32:10 +0400

    User controller login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -90,7 +90,7 @@ class UserController extends Controller
         try {
             Auth::login($checkIfExist,$request->has('remember'));
         } catch (\Throwable $th) {
-            info($th);
+
         }
 
 
