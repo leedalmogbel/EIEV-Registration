89dd21c82f5f4af159fc55e05c8d6bf1120098ee
EIEV <eiev.alwathba.notif@gmail.com>
2022-11-21 16:38:13 +0400

    add to pool

--- app/Models/Snpool.php
+++ app/Models/Snpool.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Snpool extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'userid',
+        'stableid',
+        'startno'
+    ];
 }
