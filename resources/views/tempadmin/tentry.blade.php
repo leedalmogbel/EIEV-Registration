92076544c17852c795f12f4a525c576ae6b19206
EIEV <eiev.alwathba.notif@gmail.com>
2022-11-22 13:08:57 +0400

    test tentry

--- resources/views/tempadmin/tentry.blade.php
+++ resources/views/tempadmin/tentry.blade.php
@@ -113,10 +113,11 @@
       const hid = $('.horse-select.select-2-basic').val();
       const rid = $('.rider-select.select-2-basic').val();
       console.log(href,eid,hid,rid);
-      // if(uid >0){
-      //   href = `https://registration.eiev-app.ae/${href}?params[EventID]=${eid}&params[HorseID]=${hid}&params[RiderID]=${rid}&params[UserID]=${uid}`;
-      //   window.location.href = href
-      // }
+      if(uid >0){
+        href = `https://registration.eiev-app.ae/${href}?params[EventID]=${eid}&params[HorseID]=${hid}&params[RiderID]=${rid}&params[UserID]=${uid}`;
+        console.log(href);
+        // window.location.href = href
+      }
     });
 
     $(document).on('click', '#select-data', function (e) {
