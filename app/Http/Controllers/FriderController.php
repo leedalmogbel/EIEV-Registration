e1d5ddc848dd6f28494d88cea5e3aebdffedd415
EIEV <eiev.alwathba.notif@gmail.com>
2022-11-23 10:13:11 +0400

    rider eligibility ajax

--- app/Http/Controllers/FriderController.php
+++ app/Http/Controllers/FriderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Frider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\FederationController;
 
 class FriderController extends Controller
 {
