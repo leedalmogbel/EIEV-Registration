bdd9cb4dfd0872c8ea4f4cbaa9eb069a4216d32e
Emirates International Endurance Village <eiev.alwathba.notif@gmail.com>
2022-11-24 16:02:54 +0400

    entries sync

--- routes/api.php
+++ routes/api.php
@@ -70,10 +70,10 @@ Route::domain('devregistration.eiev-app.ae')->group(function(){
     Route::get('horsecheck',[FhorseController::class,'checkEligibility']);
     Route::get('getentries',[FentryControler::class,'index']);
     Route::get('getprofiles',[UserprofileController::class,'index']);
-    Route::get('getentries',[FentryController::class,'index']);
 });
 Route::domain('localhost')->group(function(){
-    Route::get('cloudsync',[UserprofileController::class,'syncfromcloud']);
+    Route::get('profilecloudsync',[UserprofileController::class,'syncfromcloud']);
+    Route::get('entrycloudsync',[Fentry::class,'syncfromcloud']);
     Route::get('ridercheck',[FriderController::class,'checkEligibility']);
     Route::get('horsecheck',[FhorseController::class,'checkEligibility']);
     Route::get('getqrcode',[UserprofileController::class,'getQr']);
